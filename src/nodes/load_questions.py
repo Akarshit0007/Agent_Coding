f8ddac1b639c3f1048423c26cc35a748223daf0b
Akarshit0007 <akarshitmahajan0007@gmail.com>
2026-08-09 21:05:36 +0530

    Changes Made

---
+++ src/nodes/load_questions.py
@@ -0,0 +1,7 @@
+from src.state.BaseState import BaseState
+
+
+def load_questions(state:BaseState):
+    """Method to extract file name of questions and then extract questions from it and upload it into
+    BaseState Questions"""
+    return ""
