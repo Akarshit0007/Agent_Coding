1aedd946302e1f3cd53325d2a55d7c9b5085e6f8
Akarshit0007 <akarshitmahajan0007@gmail.com>
2026-08-09 13:41:38 +0530

    Modified

--- src/state/BaseState.py
+++ src/state/BaseState.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, TypedDict
 from src.state.Answers import Answers
 from src.state.EvalMetrics import EvalMetrics
 
-class BaseState:
+class BaseState(TypedDict):
     questions: List[str]
     agent_answer: Answers
     judge_answer: EvalMetrics
